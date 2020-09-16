319be4e734d8e69952cb16d214d69a1d0e63fe71
Ryan Camp <campryan@comcast.net>
2020-09-16 12:05:33 -0600

    Setup RSpec and complete first tests

---
+++ spec/card_spec.rb
@@ -0,0 +1,17 @@
+require '../lib/card'
+
+RSpec.describe Card do
+  before do
+    @card = Card.new(:diamond, 'Queen', 12)
+  end
+
+  it "exists" do
+    expect(@card).to be_an_instance_of(Card)
+  end
+
+  it "has_readable_attributes" do
+    expect(@card.suit).to eql(:diamond)
+    expect(@card.value).to eql('Queen')
+    expect(@card.rank).to eql(12)
+  end
+end
